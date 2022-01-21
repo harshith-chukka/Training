c35279c32454b42cc65a3b94b864adf91f868a7d
harshith-chukka <97505439+harshith-chukka@users.noreply.github.com>
2022-01-21 16:55:06 +0530

    Add files via upload

---
+++ Assignments/Assg_2/build/CMakeFiles/Assg_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Assg_2"
+  "Assg_2.pdb"
+  "CMakeFiles/Assg_2.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assg_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
